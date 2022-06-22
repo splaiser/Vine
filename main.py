8183e3d1a867241a10762ae2cbcce273a6c07836
splaiser <ybrbx@yandex.ru>
2022-06-22 17:18:22 +0300

    searching and count complete

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import os, os.path, glob
 from PyQt5.QtWidgets import QLineEdit,QTableWidgetItem, QAbstractItemView, QTableWidget
 from PyQt5.QtGui import QStandardItemModel,QStandardItem, QBrush
 from PyQt5.QtCore import Qt, QSortFilterProxyModel
+import pyodbc
+from collections import Counter
 
 
 def get_path():
     for file in glob.glob("*.accdb"):
         path = f"{os.getcwd()}\{file}"
-        print(path)
         return path
 
 
@@ -117,14 +118,29 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
+        self.search=QtWidgets.QLineEdit(self)
+        self.search.setGeometry(40,10,200,35)
+        self.search.setPlaceholderText('Поиск по названию')
+        self.search.textChanged.connect(self.searching)
+
+        self.search=QtWidgets.QLineEdit(self)
+        self.search.setGeometry(280,10,200,35)
+        self.search.setPlaceholderText('Поиск по синониму')
+        self.search.textChanged.connect(self.searching2)
+
+        self.search=QtWidgets.QLineEdit(self)
+        self.search.setGeometry(520,10,200,35)
+        self.search.setPlaceholderText('Поиск по номеру образца')
+        self.search.textChanged.connect(self.searching3)
+
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Вино"))
         self.pushButton_3.setText(_translate("MainWindow", "Добавить запись"))
         self.pushButton_2.setText(_translate("MainWindow", "Удалить запись"))
-        self.pushButton_4.setText(_translate('MainWindow', "Поиск записи"))
-        self.pushButton_5.setText(_translate("MainWindow", "Сбросить"))
+        self.pushButton_4.setText(_translate("MainWindow", "Места происхождения"))
+        self.pushButton_5.setText(_translate("MainWindow", "Таксономия"))
         self.pushButton.setText(_translate("MainWindow", "Выход"))
         self.pushButton_3.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
         self.pushButton_2.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
@@ -192,41 +208,49 @@ class Dialog2(QDialog):
 class Dialog3(QDialog):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('Поиск записи')
+        self.setWindowTitle('Места происхождения')
+        self.table_dialog = QtWidgets.QTableView(self)
+        self.table_dialog.setFixedSize(240,640)
+        self.table_dialog.setObjectName("dialogtableWidget")
+        self.model_dialog = QStandardItemModel(self)
+        cnxn = pyodbc.connect(r"DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};"
+                              f"DBQ={get_path()}")
+        cursor = cnxn.cursor()
+        dt = cursor.execute(
+            """
+            SELECT "Место происхождения",COUNT("Место происхождения")
+            FROM Паспорт
+            GROUP BY "Место происхождения"
+             """).fetchall()
 
-        self.line_edit_name = QLineEdit()
-        self.line_edit_alt_name = QLineEdit()
-        self.line_edit_number = QLineEdit()
-        self.line_edit_texon = QLineEdit()
-        self.line_edit_place = QLineEdit()
-        self.line_edit_bio = QLineEdit()
-        self.line_edit_pedigree_eng = QLineEdit()
-        self.line_edit_pedigree_rus = QLineEdit()
-        self.line_edit_geolocation = QLineEdit()
-        self.line_edit_storage = QLineEdit()
-        self.line_edit_gen_passport = QLineEdit()
+        for i in range(len(dt)):
+            for j in range(2):
+                self.model_dialog.setItem(i, j, QStandardItem(str(dt[i][j])))
+        self.table_dialog.setModel(self.model_dialog)
 
-        form_layout = QFormLayout()
-        form_layout.addRow('Наименование образца по русски:', self.line_edit_name)
-        form_layout.addRow('Синонимы:', self.line_edit_alt_name)
-        form_layout.addRow('Номер_образца:', self.line_edit_number)
-        form_layout.addRow('Таксономия:', self.line_edit_texon)
-        form_layout.addRow('Место происхождения:', self.line_edit_place)
-        form_layout.addRow('Биологический статус образца:', self.line_edit_bio)
-        form_layout.addRow('Родословная по английски:', self.line_edit_pedigree_eng)
-        form_layout.addRow('Родословная по-русски:', self.line_edit_pedigree_rus)
-        form_layout.addRow('Местонахождения страховых дублетов:', self.line_edit_geolocation)
-        form_layout.addRow('Типы хранения:', self.line_edit_storage)
-        form_layout.addRow('Генетический паспорт сорта:', self.line_edit_gen_passport)
+class Dialog4(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Таксономия')
+        self.table_dialog = QtWidgets.QTableView(self)
+        self.table_dialog.setFixedSize(480,640)
+        self.table_dialog.setObjectName("dialogtableWidget")
+        self.model_dialog = QStandardItemModel(self)
+        cnxn = pyodbc.connect(r"DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};"
+                              f"DBQ={get_path()}")
+        cursor = cnxn.cursor()
+        dt = cursor.execute(
+            """
+            SELECT "Таксономия                            каталог",COUNT("Таксономия                            каталог")
+            FROM Паспорт
+            GROUP BY "Таксономия                            каталог"
+             """).fetchall()
 
-        button_box = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
-        button_box.accepted.connect(self.accept)
-        button_box.rejected.connect(self.reject)
+        for i in range(len(dt)):
+            for j in range(2):
+                self.model_dialog.setItem(i, j, QStandardItem(str(dt[i][j])))
+        self.table_dialog.setModel(self.model_dialog)
 
-        main_layout = QVBoxLayout()
-        main_layout.addLayout(form_layout)
-        main_layout.addWidget(button_box)
-        self.setLayout(main_layout)
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
@@ -236,7 +260,8 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.pushButton.clicked.connect(self.close)
         self.pushButton_3.clicked.connect(self.add_row)
         self.pushButton_2.clicked.connect(self.remove_row)
-        self.pushButton_4.clicked.connect(self.searching)
+        self.pushButton_4.clicked.connect(self.place)
+        self.pushButton_5.clicked.connect(self.taxo)
         self.db = QSqlDatabase.addDatabase('QODBC')
         self.db.setDatabaseName(
             r"DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};"
@@ -263,11 +288,24 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.tableWidget.setSortingEnabled(True)
         self.tableWidget.horizontalHeader().setSectionResizeMode
 
+        self.proxy=QSortFilterProxyModel(self)
+        self.proxy.setSourceModel(self.model)
+        self.tableWidget.setModel(self.proxy)
 
+    def searching(self, text):
+        self.proxy.setFilterKeyColumn(0)
+        search = QtCore.QRegExp(text, QtCore.Qt.CaseInsensitive, QtCore.QRegExp.RegExp)
+        self.proxy.setFilterRegExp(search)
 
+    def searching2(self, text):
+        self.proxy.setFilterKeyColumn(1)
+        search = QtCore.QRegExp(text, QtCore.Qt.CaseInsensitive, QtCore.QRegExp.RegExp)
+        self.proxy.setFilterRegExp(search)
 
-
-
+    def searching3(self, text):
+        self.proxy.setFilterKeyColumn(2)
+        search = QtCore.QRegExp(text, QtCore.Qt.CaseInsensitive, QtCore.QRegExp.RegExp)
+        self.proxy.setFilterRegExp(search)
 
     def add_row(self):
         inputDialog = Dialog()
@@ -312,17 +350,17 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             msg = QMessageBox.information(self, 'Внимание', 'Выберите запись для удаления.')
             return
 
-        name = self.model.record(row).value(1)
-        alt_name = self.model.record(row).value(2)
-        number = self.model.record(row).value(3)
-        texon = self.model.record(row).value(4)
-        place = self.model.record(row).value(5)
-        bio = self.model.record(row).value(6)
-        pedigree_eng = self.model.record(row).value(7)
-        pedigree_rus = self.model.record(row).value(8)
-        geolocation = self.model.record(row).value(9)
-        storage = self.model.record(row).value(10)
-        gen_passport = self.model.record(row).value(11)
+        name = self.model.record(row).value(0)
+        alt_name = self.model.record(row).value(1)
+        number = self.model.record(row).value(2)
+        texon = self.model.record(row).value(3)
+        place = self.model.record(row).value(4)
+        bio = self.model.record(row).value(5)
+        pedigree_eng = self.model.record(row).value(6)
+        pedigree_rus = self.model.record(row).value(7)
+        geolocation = self.model.record(row).value(8)
+        storage = self.model.record(row).value(9)
+        gen_passport = self.model.record(row).value(10)
 
         inputDialog = Dialog()
         inputDialog.setWindowTitle('Удалить запись ???')
@@ -348,30 +386,14 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
         msg = QMessageBox.information(self, 'Успех', 'Запись удалена.')
 
-    def searching(self):
-        inputDialog = Dialog3()
-        rez = inputDialog.exec()
-        if not rez:
-            msg = QMessageBox.information(self, 'Внимание', 'Вы отменили поиск записи.')
-            return
-        name = inputDialog.line_edit_name.text()
-        alt_name = inputDialog.line_edit_alt_name.text()
-        number = inputDialog.line_edit_number.text()
-        texon = inputDialog.line_edit_texon.text()
-        place = inputDialog.line_edit_place.text()
-        bio = inputDialog.line_edit_bio.text()
-        pedigree_eng = inputDialog.line_edit_pedigree_eng.text()
-        pedigree_rus = inputDialog.line_edit_pedigree_rus.text()
-        geolocation = inputDialog.line_edit_geolocation.text()
-        storage = inputDialog.line_edit_storage.text()
-        gen_passport = inputDialog.line_edit_gen_passport.text()
+    def place(self):
+        tb_dialog = Dialog3()
+        rez = tb_dialog.exec()
 
-        query = QSqlQuery(self.db)
-        query.prepare(f"""SELECT * FROM Паспорт WHERE "Наименование образца по русски" = '{name}'""")
-        s = query.exec_()
-        while query.next():
-            print(query.record())
-        print(s)
+    def taxo(self):
+        tb_dialog = Dialog4()
+        rez = tb_dialog.exec()
+
 
 
 
